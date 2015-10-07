c59be4bee0866c54ceb4cebc2f7c04e042eb7c8d
zxcmehran <zxcmehran@gmail.com>
2015-10-07 20:56:53 +0330

    Initial Release

---
+++ README.md
@@ -0,0 +1,33 @@
+# Python HTTP Server w/ User Authentication and HTTPS
+
+----
+## What's different?
+You might be familiar with [SimpleHTTPServer](https://docs.python.org/2/library/simplehttpserver.html) module already. It will give you a simple and easy to use solution for hosting content by just typing `python -m SimpleHTTPServer` in a terminal.
+
+Here is a revised version of SimpleHTTPServer with HTTPS and User Authentication support. 
+
+----
+## Installing
+You just need a clone:
+
+    $ git clone https://github.com/zxcmehran/SecurePyServer
+
+----
+## Usage
+
+    $ ./SecureHTTPServer.py [webroot] [host:port] [username:password] [certfile] [keyfile]
+`webroot` is the path to web root, relative to current working directory. You can specify a dot "." to use current directory.
+
+There is no need to specify both host and port in `host:port`. You might want to enter port or host only. Default value is 8000.
+
+Specify a Username and Password as `username:password` for **HTTP Basic Authentication**. Specifying ":" will disable authentication. Disabled by default.
+
+You can activate **HTTPS** using a Certificate and Key pair. Use `certfile` and `keyfile` to set paths.
+
+Certificate file should contain PEM encoded version of certificates chain as described in [Python SSL Documentation](https://docs.python.org/2/library/ssl.html#certificate-chains).
+
+Key file argument can be omitted if the key is included in certificate file.
+
+----
+## License
+[MIT](https://tldrlegal.com/license/mit-license) License
---
+++ SecureHTTPServer.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+
+"""
+Simple Python HTTP Server with HTTPS and basic authorization support.
+See --help for usage guide.
+
+Author: Mehran Ahadi <mehran@ahadi.me>
+Homepage: http://mehran.ahadi.me/
+License: MIT
+
+Version 1.0
+"""
+
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import sys
+import os
+import base64
+import ssl
+ 
+key = ''
+realm = 'Private Server'
+certfile = ''
+keyfile = ''
+ssl_version = ssl.PROTOCOL_TLSv1
+cwd = ''
+
+class AuthHandler(SimpleHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+ 
+    def do_AUTHHEAD(self):
+	global realm;
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"'+realm+'\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+ 
+    def do_GET(self):
+        global key
+        if self.headers.getheader('Authorization') == None:
+            self.do_AUTHHEAD()
+            self.wfile.write('Please specify a username and password.')
+            pass
+        elif self.headers.getheader('Authorization') == 'Basic '+key:
+            SimpleHTTPRequestHandler.do_GET(self)
+            pass
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(self.headers.getheader('Authorization'))
+            self.wfile.write('You are not authenticated.')
+            pass
+ 
+def test(HandlerClass = AuthHandler,
+         ServerClass = BaseHTTPServer.HTTPServer):
+
+    global ssl_version, certfile, keyfile, key;
+    
+    if key == '':
+    	print 'No Authorization needed.'
+    	HandlerClass = SimpleHTTPRequestHandler
+    else:
+    	print 'Authorization is required.'
+    
+    protocol = "HTTP/1.1"
+    host = ''
+    port = 8000
+    if len(sys.argv) > 2:
+        arg = sys.argv[2]
+        if ':' in arg:
+            host, port = arg.split(':')
+            port = int(port)
+        else:
+            try:
+                port = int(sys.argv[2])
+            except:
+                host = sys.argv[2]
+
+    server_address = (host, port)
+    HandlerClass.protocol_version = protocol
+    
+    try:
+        httpd = ServerClass(server_address, HandlerClass)
+    except Exception, e:
+    	print e
+    	sys.exit(2)
+    
+    try:
+        if certfile != '' :
+	    httpd.socket = ssl.wrap_socket (httpd.socket, certfile=certfile, keyfile=keyfile, server_side=True, ssl_version=ssl_version)
+    except Exception, e:
+    	print e
+    	sys.exit(3)	    
+	
+    sa = httpd.socket.getsockname()
+    if certfile != '':
+    	print 'Serving HTTPS on', sa[0], "port", sa[1], "..."
+    else:
+    	print 'Serving HTTP on', sa[0], "port", sa[1], "..."
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print "\nTerminating...\n"
+        sys.exit(1)
+ 
+ 
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '--help':
+        print "\nSimple HTTP Server with HTTPS and Authorization support."
+        print "Version 1.0\n"
+        print "Usage: ./SecureHTTPServer.py [webroot] [host:port] [username:password] [certfile] [keyfile]\n"
+        print 'Notes:'
+        print '\t* Specify dot "." as [webroot] to use current directory. Default \n\tvalue is dot "."'
+        print '\t* There is no need to specify both host and port. You might want to\n\tenter port or host only. Default value is 8000.'
+        print '\t* Specifying ":" as [username:password] will disable authorization.\n\tDisabled by default.'
+        print '\t* Certificate file should contain PEM encoded version of certificate\n\tchain as described in:'
+        print '\t\t https://docs.python.org/2/library/ssl.html#certificate-chains'
+        print '\t* Key file argument can be omitted if the key is included in certificate\n\tfile.'
+        print '\t* You might need to run this script as root.'
+        print '\nAuthor: Mehran Ahadi <mehran@ahadi.com>';
+        print 'Author Homepage: http://mehran.ahadi.me/';
+        sys.exit(0)
+    print 'See --help for more info.';
+    
+    cwd = os.getcwd();
+    
+    if len(sys.argv) > 1:
+        try:
+            os.chdir(sys.argv[1])
+        except Exception, e:
+            print 'Error changing directory:', e;
+            exit(4)
+    
+    if len(sys.argv) > 3 and sys.argv[3] != ':':
+        key = base64.b64encode(sys.argv[3])
+        
+    if len(sys.argv) > 4:
+        certfile = sys.argv[4]
+        if not os.path.isabs(certfile) :
+            certfile = cwd + '/' + certfile
+            
+        if not os.path.exists(certfile) :
+            print "Certificate File %s not found." % certfile
+    
+    if len(sys.argv) > 5:
+        keyfile = sys.argv[5]
+        if not os.path.isabs(keyfile) :
+            keyfile = cwd + '/' + keyfile
+        
+        if not os.path.exists(keyfile) :
+            print "Key File %s not found." % keyfile
+    
+    test()
