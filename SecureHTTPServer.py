99a4e675b4138bc32f47fa2fe2236c49bed9c581
zxcmehran <zxcmehran@gmail.com>
2015-10-11 20:50:32 +0330

    Added ThreadMixIn, Log display flag and fixed HTTP/1.1 locking out

--- SecureHTTPServer.py
+++ SecureHTTPServer.py
@@ -4,7 +4,7 @@
 Simple Python HTTP Server with HTTPS and basic authorization support.
 See --help for usage guide.
 
-Author: Mehran Ahadi <mehran@ahadi.me>
+Author: Mehran Ahadi
 Homepage: http://mehran.ahadi.me/
 License: MIT
 Repository: https://github.com/zxcmehran/SecurePyServer
