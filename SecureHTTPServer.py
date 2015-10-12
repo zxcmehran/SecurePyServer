23dc4ed72784f7bceb7467c8acb1d855209a90fc
zxcmehran <zxcmehran@gmail.com>
2015-10-12 16:19:39 +0330

    Better parameter detection

--- SecureHTTPServer.py
+++ SecureHTTPServer.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 3 and sys.argv[3] != ':':
         key = base64.b64encode(sys.argv[3])
         
-    if len(sys.argv) > 4:
+    if len(sys.argv) > 4 and sys.argv[4] != '':
         certfile = sys.argv[4]
         if not os.path.isabs(certfile) :
             certfile = cwd + '/' + certfile
@@ -163,7 +163,7 @@ if __name__ == '__main__':
         if not os.path.exists(certfile) :
             print "Certificate File %s not found." % certfile
     
-    if len(sys.argv) > 5:
+    if len(sys.argv) > 5 and sys.argv[5] != '':
         keyfile = sys.argv[5]
         if not os.path.isabs(keyfile) :
             keyfile = cwd + '/' + keyfile
